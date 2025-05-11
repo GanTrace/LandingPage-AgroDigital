274f785b8c0f2ce9102caecff4cf34e4a082c684
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-05-11 03:33:20 -0500

    feat: add event listeners for navigation and hero button

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById('nav-start').onclick = function() {
+    window.location.href = ''; 
+  };
+  document.getElementById('hero-button').onclick = function() {
+    window.location.href = ''; 
+  };
+  
+  
