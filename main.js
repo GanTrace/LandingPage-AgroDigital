3632e834c69f42d37e1db0809ea53c24b060d29d
Rodrigo S <rodrigojsr@upc.edu.pe>
2025-05-14 11:49:12 -0500

    chore: add launch configuration and update redirect URLs

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 document.getElementById('nav-start').onclick = function() {
-    window.location.href = ''; 
-  };
+    window.location.href = 'https://gantrace.github.io/AgroDigital--FrontendApp/'; 
+};
+
 document.getElementById('hero-button').onclick = function() {
-    window.location.href = ''; 
-  };
+    window.location.href = 'https://gantrace.github.io/AgroDigital--FrontendApp/'; 
+};
   
 document.querySelector('.menu-toggle').addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('open');
-  });
+});
